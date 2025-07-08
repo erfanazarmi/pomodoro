0895f6ac3c46bdb1f029b1f26e3f7205c4a5abd0
Erfan Azarmi <erfanazarmi1@gmail.com>
2025-07-08 22:31:04 +0330

    feat: set up state management

---
+++ src/store/store.ts
@@ -0,0 +1,94 @@
+import { create } from "zustand";
+
+type Mode = "session" | "break";
+
+interface Settings {
+  breakLength: number;
+  sessionLength: number;
+  displayTime: number;
+  isPlaying: boolean;
+  currentMode: Mode;
+}
+
+interface Actions {
+  resetState: () => void;
+  togglePlay: () => void;
+  setDisplayTime: (time: number) => void;
+  setCurrentMode: (mode: Mode) => void;
+  incrementTime: (mode: Mode) => void;
+  decrementTime: (mode: Mode) => void;
+  setTime: (mode: Mode, value: number) => void;
+}
+
+type State = Settings & Actions;
+
+const defaultSettings: Settings = {
+  breakLength: 5,
+  sessionLength: 25,
+  displayTime: 25 * 60,
+  isPlaying: false,
+  currentMode: "session",
+};
+
+export const useStore = create<State>((set) => ({
+  ...defaultSettings,
+
+  resetState: () => set(() => ({ ...defaultSettings })),
+
+  togglePlay: () => set((state) => ({ isPlaying: !state.isPlaying })),
+
+  setDisplayTime: (time) => set(() => ({ displayTime: time })),
+
+  setCurrentMode: (mode) => set(() => ({ currentMode: mode })),
+
+  incrementTime: (mode) =>
+    set((state) => {
+      const newState = { ...state };
+      if (mode === "session") {
+        newState.sessionLength += 1;
+        if (state.currentMode === "session") {
+          newState.displayTime = newState.sessionLength * 60;
+        }
+      } else if (mode === "break") {
+        newState.breakLength += 1;
+        if (state.currentMode === "break") {
+          newState.displayTime = newState.breakLength * 60;
+        }
+      }
+      return newState;
+    }),
+
+  decrementTime: (mode) =>
+    set((state) => {
+      const newState = { ...state };
+      if (mode === "session" && state.sessionLength > 1) {
+        newState.sessionLength -= 1;
+        if (state.currentMode === "session") {
+          newState.displayTime = newState.sessionLength * 60;
+        }
+      } else if (mode === "break" && state.breakLength > 1) {
+        newState.breakLength -= 1;
+        if (state.currentMode === "break") {
+          newState.displayTime = newState.breakLength * 60;
+        }
+      }
+      return newState;
+    }),
+
+  setTime: (mode, value) =>
+    set((state) => {
+      const newState = { ...state };
+      if (mode === "session") {
+        newState.sessionLength = value;
+        if (state.currentMode === "session") {
+          newState.displayTime = value * 60;
+        }
+      } else if (mode === "break") {
+        newState.breakLength = value;
+        if (state.currentMode === "break") {
+          newState.displayTime = value * 60;
+        }
+      }
+      return newState;
+    }),
+}));
